12f90e2fb1a68c5d9450e5b030fcbc6e805d5666
JanDuero <jduero@placester.com>
2021-08-26 16:49:22 +0800

    Add unit testing

--- PaymentSystemUnitTest/PaymentTest.cs
+++ PaymentSystemUnitTest/PaymentTest.cs
@@ -1,5 +1,6 @@
 using PS.Application.Services;
 using System;
+using System.Linq;
 using Xunit;
 
 namespace PaymentSystemUnitTest
@@ -33,6 +34,34 @@ namespace PaymentSystemUnitTest
             //Assert
             Assert.Equal(result.AcctTransactions.Count , expected);
         }
+
+        [Fact]
+        public void TransactionAmount()
+        {
+            //Arr
+            int expected = 10000;
+            TransactionService transactionService = new TransactionService();
+
+            //Act
+            var result = transactionService.GetAcctBalance();
+
+            //Assert
+            Assert.True(result.AcctTransactions.Select(i => i.Amount).FirstOrDefault() >= expected);
+        }
+
+        [Fact]
+        public void TransactionDate()
+        {
+            //Arr
+            int expected = DateTime.UtcNow.Year;
+            TransactionService transactionService = new TransactionService();
+
+            //Act
+            var result = transactionService.GetAcctBalance();
+
+            //Assert
+            Assert.True(result.AcctTransactions.Select(i => i.Date.Year).FirstOrDefault() >= expected);
+        }
         #endregion
 
         #region Negative
@@ -61,6 +90,34 @@ namespace PaymentSystemUnitTest
 
             //Assert
             Assert.False(result.AcctTransactions.Count == expected);
+        }
+
+        [Fact]
+        public void TransactionAmountNegative()
+        {
+            //Arr
+            int expected = 90000;
+            TransactionService transactionService = new TransactionService();
+
+            //Act
+            var result = transactionService.GetAcctBalance();
+
+            //Assert
+            Assert.False(result.AcctTransactions.Select(i => i.Amount).FirstOrDefault() <= expected);
+        }
+
+        [Fact]
+        public void TransactionDateNegative()
+        {
+            //Arr
+            int expected = DateTime.UtcNow.AddYears(-2).Year;
+            TransactionService transactionService = new TransactionService();
+
+            //Act
+            var result = transactionService.GetAcctBalance();
+
+            //Assert
+            Assert.True(result.AcctTransactions.Select(i => i.Date.Year).FirstOrDefault() >= expected);
         }
         #endregion
     }
