538b1df3ee8a2eb4806eefd595a5d24e5fdb10bf
JanDuero <jduero@placester.com>
2021-08-26 11:57:41 +0800

    Add project files.

---
+++ PaymentSystemUnitTest/PaymentTest.cs
@@ -0,0 +1,14 @@
+using System;
+using Xunit;
+
+namespace PaymentSystemUnitTest
+{
+    public class PaymentTest
+    {
+        [Fact]
+        public void Test1()
+        {
+
+        }
+    }
+}
