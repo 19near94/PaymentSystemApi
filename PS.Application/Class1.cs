ce6d06f4044be4ad00bdb333d431a8284ee1276d
JanDuero <jduero@placester.com>
2021-08-26 12:15:50 +0800

    Add entities

--- PS.Application/Class1.cs
+++
@@ -1,8 +0,0 @@
-﻿using System;
-
-namespace PS.Application
-{
-    public class Class1
-    {
-    }
-}
---
+++ PS.Domain/Entities/AcctTransaction.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace PS.Domain.Entities
+{
+    public class AcctTransaction
+    {
+        public double Amount { get; set; }
+
+        public DateTime Date { get; set; }
+
+        public string Status { get; set; }
+
+    }
+}
